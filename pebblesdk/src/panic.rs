28d4771fc478339aaa2e1a91b0059322fdf873eb
jae beller <foss@jae.zone>
2025-03-07 22:09:56 -0500

    pebblesdk: Print app name in panic message

--- pebblesdk/src/panic.rs
+++ pebblesdk/src/panic.rs
@@ -1,20 +1,34 @@
 use core::arch::asm;
+use core::cmp::min;
 use core::panic::PanicInfo;
 
-use pebblesdk_sys::{app_log, APP_LOG_LEVEL_ERROR};
+use pebblesdk_sys::{app_log, appinfo::APP_INFO, APP_LOG_LEVEL_ERROR};
+
+const FILE_BUFFER_LENGTH: usize = 64;
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    let location = info.location();
+    let mut file_buffer: [u8; FILE_BUFFER_LENGTH] = [0; FILE_BUFFER_LENGTH];
+    let mut line = 0;
+    if let Some(location) = info.location() {
+        let file = location.file().as_bytes();
+        let n = min(file.len(), FILE_BUFFER_LENGTH - 1);
+        file_buffer[..n].copy_from_slice(&file[file.len() - n..file.len()]);
+        line = location.line() as i32;
+    } else {
+        file_buffer[0] = '?' as u8;
+    }
+
     unsafe {
         app_log(
             APP_LOG_LEVEL_ERROR as u8,
-            location.map(|loc| loc.file()).unwrap_or("???\0").as_ptr(),
-            location.map(|loc| loc.line() as i32).unwrap_or(0),
-            c"app panicked".as_ptr(),
+            file_buffer.as_ptr(),
+            line,
+            c"app '%s' panicked".as_ptr(),
+            &raw const APP_INFO.name,
         );
-
-        // Trigger a UsageFault exception so the kernel kills our process.
-        asm!("udf 0", options(noreturn));
     }
+
+    // Trigger a UsageFault exception so the kernel kills our process.
+    unsafe { asm!("udf 0", options(noreturn)) }
 }
